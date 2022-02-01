db4f9e8006853cfce894cd8f9a2db9988619a3ed
chanhong-ji <jichanhong24@gmail.com>
2022-02-01 15:26:13 +0900

    Connect mongoDB

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
 import path from "path";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import "./db.js";
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
-const onListening = () => console.log(`Server on ${PORT}`);
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+});
 app.use(morgan("dev"));
 app.use("/api/check", (req, res, next) => {
   res.json({ message: "message from server" });
@@ -15,8 +23,9 @@ app.use("/api/check", (req, res, next) => {
 });
 app.use(express.static(path.join(__dirname, "react-project/build")));
 
+// Route
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "react-project/build/index.html"));
 });
 
-app.listen(PORT, onListening);
+app.listen(PORT, () => console.log(`Server on ${PORT}`));
