12dcba47d9c4598030f69ff58aed6ceca035b9e0
chanhong-ji <jichanhong24@gmail.com>
2022-02-03 22:58:18 +0900

    Create Homepage outline

--- react-project/src/components/Home.js
+++ react-project/src/components/Home.js
@@ -1,9 +1,36 @@
-import { useQuery } from "react-query";
-import { fetchData } from "../api";
+import { Link, useHistory, useRouteMatch } from "react-router-dom";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import Header from "./Header";
+
+const Main = styled.main``;
+
+const Btn = styled.button``;
+
+const Section = styled.section``;
+
+const Title = styled.h3``;
+
+const Shelve = styled.div``;
+
+const Main2 = styled.main``;
+
+const Aside = styled.aside``;
 
 function Home() {
-  const { isLoading, data } = useQuery("data", fetchData);
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <Main>
+        <Btn>+</Btn>
+        <Section>
+          <Title></Title>
+          <Shelve></Shelve>
+        </Section>
+      </Main>
+      <Aside></Aside>
+    </>
+  );
 }
 
 export default Home;
